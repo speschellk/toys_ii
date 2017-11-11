3bfda29a9a0db3427dec1b7130132c55b62d99c6
Kay Christensen <kaychristensen@gmail.com>
2017-11-10 16:04:10 -0800

    Finish set and set spec.

--- classes/set.js
+++ classes/set.js
@@ -1,24 +1,26 @@
-class Set {
+module.exports = class Set {
   constructor() {
-    this.values = [];
+    this.__values = [];
     this.size = 0;
   }
 
   add(value) {
-    if (!this.values.contains(value)) {
-      this.values.push(value);
+    if (!this.has(value)) {
+      this.__values.push(value);
       this.size++;
+    } else {
+      return false;
     }
   }
 
   clear() {
-    this.values = [];
+    this.__values = [];
   }
 
   delete(target) {
-    for (let i = 0; i < this.values.length; i ++) {
-      if (this.values[i] === target) {
-        this.values.splice(i, 1);
+    for (let i = 0; i < this.__values.length; i ++) {
+      if (this.__values[i] === target) {
+        this.__values.splice(i, 1);
         return true;
       }
     }
@@ -27,30 +29,30 @@ class Set {
 
   entries() {
     const entries = [];
-    for (let value of this.values) {
+    for (let value of this.__values) {
       entries.push([value, value]);
     }
     return entries;
   }
 
   forEach(cb, thisArg) {
-    for (let i = 0; i < this.values.length; i++) {
+    for (let i = 0; i < this.__values.length; i++) {
       if (thisArg) {
-        cb.call(thisArg, this.values[i], i, this);
+        cb.call(thisArg, thisArg[i], i);
       } else {
-        cb(this.values[i], i, this);
+        cb(this.__values[i], i, this);
       }
     }
   }
 
   has(target) {
-    if (this.values.contains(target)) {
+    if (this.__values.includes(target)) {
       return true;
     }
     return false;
   }
 
   values() {
-    return this.values;
+    return this.__values;
   }
 }
