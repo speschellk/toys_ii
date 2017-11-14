d92e86179eba9b923195f98f086a26d0a6c69754
Kay Christensen <kaychristensen@gmail.com>
2017-11-13 16:19:08 -0800

    Add heap sorting solution.

--- js/strings/__tests__/isUnique.spec.js
+++ js/strings/__tests__/isUnique.spec.js
@@ -1,4 +1,4 @@
-const { isUnique, isUniqueSet } = require('../isUnique');
+const { isUnique, isUniqueSet, isUniqueSort } = require('../isUnique');
 
 describe('isUnique', () => {
   test('Returns null when given no string as input', () => {
@@ -47,5 +47,31 @@ describe('isUniqueSet', () => {
   test('Returns false for strings with repeated non-letter characters', () => {
     let str = '(123)) 456-7890';
     expect(isUniqueSet(str)).toEqual(false);
+  });
+});
+
+describe('isUniqueSort', () => {
+  // test cases must not be nearly sorted in order to use heap sort
+  test('Returns null when given no string as input', () => {
+    expect(isUniqueSort()).toBeNull();
+  });
+  test('Returns null when given empty string as input', () => {
+    expect(isUniqueSort('')).toBeNull();
+  });
+  test('Returns true for strings with all unique letters', () => {
+    let str = 'ag';
+    expect(isUniqueSort(str)).toEqual(true);
+  });
+  test('Returns true for strings with unique non-letter characters', () => {
+    let str = '%$+~`.';
+    expect(isUniqueSort(str)).toEqual(true);
+  });
+  test('Returns false for strings with repeated letters', () => {
+    let str = 'ax2d-%a20';
+    expect(isUniqueSort(str)).toEqual(false);
+  });
+  test('Returns false for strings with repeated non-letter characters', () => {
+    let str = '()))(()(()()()()';
+    expect(isUniqueSort(str)).toEqual(false);
   });
 });
--- js/strings/isUnique.js
+++ js/strings/isUnique.js
@@ -1,10 +1,11 @@
+const { HeapSort } = require('../../sorts/heap');
 /*
 Implement an algorithm to determine if a string has all unique characters.
 What if you cannot use additional data structures?
 
 I: string of characters
 O: boolean indicating whether or not string contains only unique characters
-C: none
+C: upper and lower case letters are not the same
 E: no string, empty string
 */
 
@@ -43,4 +44,23 @@ function isUnique(str) {
   return true;
 }
 
-module.exports = { isUniqueSet, isUnique };
+/* ---------------------------------------------------------------------------------- */
+
+// C: better than quadratic time and no extra space
+
+// Time: O(n + n log(n)) wrt str length
+// Space: O(1)
+function isUniqueSort(str) {
+  if (!str) return null;
+
+  // use heap sort for O(n log(n)) time and O(1) space
+  str = new HeapSort(str).heapSort(str);
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+module.exports = { isUniqueSet, isUnique, isUniqueSort };
